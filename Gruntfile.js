4175c38172ba1f9c7d77a1c89526c5417897269f
Ernesto Celis <developer@celisdelafuente.net>
2017-10-09 17:48:23 -0500

    Gruntfile decaffeinated

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,22 +5,14 @@
  */
 module.exports = grunt => {
   grunt.initConfig({
-    coffee: {
-      compile: {
-        files: [
-          {expand: true, cwd: 'src', src: '*.coffee', dest: 'lib', ext: '.js'}
-        ]
-      }
-    },
     mochaTest: {
       options: {
         reporter: 'spec'
       },
-      src: ['test/*.coffee']
+      src: ['test/*.js']
     }});
 
-  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-mocha-test');
 
-  return grunt.registerTask('default', ['coffee', 'mochaTest']);
+  return grunt.registerTask('default', ['mochaTest']);
 };
